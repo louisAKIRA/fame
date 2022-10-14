a6b7a1b9fff395cfa55113d4551e4ba87ebcc02c
louis <louis.saga5@gmail.com>
2022-10-14 22:23:06 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection("user").snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, i) {
+                          var data = snapshot.data!.docs[i];
+                          return Text(data['ที่อยู่']);
+                        });
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
