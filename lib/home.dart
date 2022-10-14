f76453ba7bd5ea1352eaec0d15180a661f923aa1
louis <louis.saga5@gmail.com>
2022-10-14 23:18:13 +0700

    gg

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
